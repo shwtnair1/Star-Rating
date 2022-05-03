3e14085dd9292d0d0209cd6f824304a87abe2f35
shwtnair1 <shwt.nair1@gmail.com>
2022-05-03 19:09:17 +0530

    Build Star rating component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { useState } from "react";
+import "./App.css";
+const App = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const handleClick = (rate) => {
+    setRating(rate);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {[...Array(5)].map((item, i) => {
+        i += 1;
+        return (
+          <button
+            key={i}
+            type="button"
+            onClick={() => handleClick(i)}
+            onMouseEnter={() => setHover(i)}
+            onMouseLeave={() => setHover(rating)}
+          >
+            {i <= (hover || rating) ? (
+              <span className="star">&#9733;</span>
+            ) : (
+              <span className="star">&#9734;</span>
+            )}
+          </button>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default App;
